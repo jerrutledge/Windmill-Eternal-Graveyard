2b7f9feb2d5d992df18e5efe412b4fbfc27ba487
Jeremy Rutledge <191650357+ludovancy@users.noreply.github.com>
2025-06-14 14:43:53 -0300

    Main scene setup

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,11 +3,3 @@
 extends Node2D
 
 var score : int = 0
-
-# Adds 1 to score variable
-func add_score():
-	score += 1
-
-# Loads next level
-func load_next_level(next_scene : PackedScene):
-	get_tree().change_scene_to_packed(next_scene)
