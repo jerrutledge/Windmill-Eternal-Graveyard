1e66ae24b2697a527d0270492023902d5c2c262c
Jeremy Rutledge <jerrutledge@gmail.com>
2025-06-14 15:34:22 -0300

    Add customizable windmill speed

--- Scripts/windmill.gd
+++ Scripts/windmill.gd
@@ -1,3 +1,10 @@
 extends CharacterBody2D
 
 @export var rotate : bool = false
+@export var speed : float = 1.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	if rotate and speed > 0:
+		animation_player.play("rotor")
+		animation_player.speed_scale = speed
